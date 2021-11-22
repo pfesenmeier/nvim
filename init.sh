0d7b2cdc2d803f374fbf2d5140d78b02873c1d37
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-11-22 09:50:06 -0500

    soft link .bashrc

--- init.sh
+++ init.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
-NVIM="$HOME/nvim"
-rm ~/.bashrc
-cp ~/nvim/dotbashrc ~/.bashrc 
-source ~/.bashrc
+NVIM=$HOME/nvim
+NVIM_CONFIG=$HOME/.config/nvim
 
-# bash $NVIM/cargo_install.sh
+bash $NVIM/cargo_install.sh
+
+# no quiet option for ln
+rm $NVIM_CONFIG/init.lua
+rm -r $NVIM_CONFIG/lua
+rm $HOME/.bashrc
+
+ln -s $NVIM/init.lua $NVIM_CONFIG/init.lua
+ln -s $NVIM/lua $NVIM_CONFIG/lua
+ln -s $NVIM/dotbashrc $HOME/.bashrc
 
