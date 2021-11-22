d00236f067b9694e50af3f1358b84a4c6ba26d37
Paul Fesenmeier <pfesenmeier@gmail.com>
2021-11-21 21:13:53 -0500

    autosave (via script)

--- init.lua
+++ init.lua
@@ -46,31 +46,18 @@ require "paq" {
    "MordechaiHadad/nvim-papadark";
 }
 
-vim.cmd('syntax on')
-vim.cmd('set termguicolors')
-vim.cmd('colorscheme papadark')
-vim.cmd('set number')
 
 local settings_dir = "lua/"
 require(settings_dir .. "rust_analyzer")
 require(settings_dir .. "compare")
+require(settings_dir .. "settings")
 
--- tabs are spaces
-opt.tabstop = 2 -- tabs are tabstop spaces long
-opt.shiftwidth = 2 -- indents are 2 widths long
-opt.softtabstop = 4 -- colunmn??
-cmd('set expandtab') -- tabs are now spaces
 
 
 
 -- notes from https://github.com/jonhoo/configs/blob/master/editor/.config/nvim/init.vim
--- https://www.youtube.com/watch?v=ycMiMDHopNc&t=3791s
--- plugged 
--- nvim +PlugIntall +PlugClean
 -- light line??
--- ale for.... no matter
 -- yank highliht
--- vim rooter
 -- fzf - fuzzy search
 -- leader character 'space', 'space' 'space' to switch to file I was just in 
 -- Language Client?? - do not need it , ound
---
+++ lua/settings.lua
@@ -0,0 +1,12 @@
+-- tabs are spaces
+vim.opt.tabstop = 2 -- tabs are tabstop spaces long
+vim.opt.shiftwidth = 2 -- indents are 2 widths long
+vim.opt.softtabstop = 4 -- colunmn??
+vim.cmd('set expandtab') -- tabs are now spaces
+
+-- theme stuff
+vim.cmd('syntax on')
+vim.cmd('set termguicolors')
+vim.cmd('colorscheme papadark')
+vim.cmd('set number')
+  
