83f40ef71bf47ca2dd35ccefc8b3d8ec9197409c
Paul Fesenmeier <fesenmeier_paul@network.lilly.com>
2021-12-15 17:36:04 -0500

    use literal string for command

--- aliases.sh
+++ aliases.sh
@@ -8,6 +8,6 @@ alias c="git commit"
 alias n=nvim
 alias cat=bat
 alias find=fd
-alias git-home="cd `git rev-parse --show-toplevel`"
+alias git-home='cd `git rev-parse --show-toplevel`'
 git config --global alias.whereami "rev-parse --abbrev-ref HEAD"
 
