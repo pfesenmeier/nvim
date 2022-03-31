3ef538b831d6c013a3b7bb5c589071fbcddf35eb
Paul Fesenmeier <pbfesenmeier@sep.com>
2022-03-31 13:28:12 -0400

    add git alias

--- aliases.fish
+++ aliases.fish
@@ -6,10 +6,10 @@ alias s="git status"
 alias a="git add"
 alias c="git commit"
 alias e=nvim
+alias g=git
 alias python=python3
 alias cat=bat
 alias find=fd
 alias git-home='cd `git rev-parse --show-toplevel`'
 git config --global alias.whereami "rev-parse --abbrev-ref HEAD"
 alias clion=clion64.exe
-
