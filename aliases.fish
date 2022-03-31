48304322149ce97c600dfc0f7760e68ae0b5bae0
Paul Fesenmeier <pfesenmeier@gmail.com>
2022-03-30 16:17:07 -0900

    save clion

--- aliases.fish
+++ aliases.fish
@@ -11,5 +11,5 @@ alias cat=bat
 alias find=fd
 alias git-home='cd `git rev-parse --show-toplevel`'
 git config --global alias.whereami "rev-parse --abbrev-ref HEAD"
-alias clion64.exe=clion
+alias clion=clion64.exe
 
