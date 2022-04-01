1cf6b27f111e616b3443767e592185bc0f03b2e7
Paul Fesenmeier <pbfesenmeier@sep.com>
2022-04-01 11:21:05 -0400

    remove recent change

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -42,7 +42,6 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>' opts)
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
