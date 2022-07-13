05f68f735152de389518e552a439ee53f58923a3
Paul Fesenmeier <pbfesenmeier@sep.com>
2022-07-13 14:53:41 -0400

    add broot

---
+++ install/broot.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+wget https://dystroy.org/broot/download/x86_64-linux/broot
+sudo mv -f broot /usr/local/bin/
+chmod u+x /usr/local/bin/broot
+
+# TODO download completions
+# https://dystroy.org/broot/download/completion/br.fish
+
