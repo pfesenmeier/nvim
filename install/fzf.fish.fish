c467c0d613dacaaf7f060f075a6073a571cb39ed
Paul Fesenmeier <pbfesenmeier@sep.com>
2022-07-13 09:14:14 -0400

    update packages for fzf.fish

---
+++ install/fzf.fish.fish
@@ -0,0 +1,5 @@
+#! /usr/bin/env fish
+
+# need fish, fzf, fd, bat
+fisher install PatrickF1/fzf.fish
+
--- install/fzf.sh
+++ install/fzf.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env bash
 
-sudo apt-get install fzf
+# sudo apt-get install fzf
+
+# for older versions
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 
