9e16b7566d1948d963d6e532326abee1a1f7a88b
Paul Fesenmeier <pfesenmeier@gmail.com>
2024-08-30 13:40:35 -0400

    toggle diagnostic

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -30,6 +30,11 @@ return {
                 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
                 vim.keymap.set({ 'v', 'n' }, '<leader>ca', vim.lsp.buf.code_action, bufopts)
                 vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+
+                vim.keymap.set('n', '<leader>dt',
+                    function()
+                        vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+                    end, bufopts)
                 vim.keymap.set('n', '<leader>f', function()
                     vim.lsp.buf.format { async = true }
                 end, bufopts)
