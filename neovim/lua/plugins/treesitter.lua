e44ec677ac90dad17ec24c4b4af576af5707630c
Paul Fesenmeier <pfesenmeier@gmail.com>
2024-09-10 11:48:14 -0400

    windows improvements

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -87,6 +87,7 @@ return {
                 "markdown",
                 "toml",
                 "typescript",
+                "terraform",
                 "hcl",
                 "html",
                 "regex",
