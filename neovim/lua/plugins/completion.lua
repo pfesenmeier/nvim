7b77e2baa5aa5660f30405f4e605dc9a98e67c9c
Paul Fesenmeier <pfesenmeier@gmail.com>
2024-09-30 15:22:43 -0400

    latest windows fixes

--- neovim/lua/plugins/completion.lua
+++ neovim/lua/plugins/completion.lua
@@ -1,7 +1,7 @@
 -- completion
 return {
     "hrsh7th/nvim-cmp",
-    lazy = true,
+    lazy = false,
     event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
         { "hrsh7th/cmp-nvim-lsp", dependencies = { 'neovim/nvim-lspconfig' } },
