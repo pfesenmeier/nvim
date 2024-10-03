4bab39f956d54c4660dfd7d0f5c6f0e988d94fd4
Paul Fesenmeier <pfesenmeier@gmail.com>
2024-10-03 12:54:41 -0400

    move lsp to csharp.lua

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -1,3 +1,5 @@
+local csharp = require"pfes.csharp"
+
 return {
     {
         "neovim/nvim-lspconfig",
@@ -71,6 +73,8 @@ return {
                 }
             end
 
+            csharp.addToLspConfig(opts, capabilities, on_attach);
+
             opts.ts_ls.setup {
                 init_options = {
                     plugins = {
@@ -90,63 +94,6 @@ return {
                 on_attach = on_attach
             }
 
-            local omnisharp_dll
-
-            if Env.islinux then
-                omnisharp_dll = Env.home .. "/.local/bin/OmniSharp/omnisharp-roslyn/OmniSharp.dll"
-            else
-                omnisharp_dll = Env.home .. "/AppData/Local/OmniSharp/omnisharp-roslyn/OmniSharp.dll"
-            end
-
-            opts.omnisharp.setup {
-
-                handlers = {
-                    ["textDocument/definition"] = require('omnisharp_extended').handler,
-                },
-                cmd = { "dotnet", omnisharp_dll },
-
-                settings = {
-                    FormattingOptions = {
-                        -- Enables support for reading code style, naming convention and analyzer
-                        -- settings from .editorconfig.
-                        EnableEditorConfigSupport = true,
-                        -- Specifies whether 'using' directives should be grouped and sorted during
-                        -- document formatting.
-                        OrganizeImports = true,
-                    },
-                    MsBuild = {
-                        -- If true, MSBuild project system will only load projects for files that
-                        -- were opened in the editor. This setting is useful for big C# codebases
-                        -- and allows for faster initialization of code navigation features only
-                        -- for projects that are relevant to code that is being edited. With this
-                        -- setting enabled OmniSharp may load fewer projects and may thus display
-                        -- incomplete reference lists for symbols.
-                        LoadProjectsOnDemand = nil,
-                    },
-                    RoslynExtensionsOptions = {
-                        -- Enables support for roslyn analyzers, code fixes and rulesets.
-                        EnableAnalyzersSupport = true,
-                        -- Enables support for showing unimported types and unimported extension
-                        -- methods in completion lists. When committed, the appropriate using
-                        -- directive will be added at the top of the current file. This option can
-                        -- have a negative impact on initial completion responsiveness,
-                        -- particularly for the first few completion sessions after opening a
-                        -- solution.
-                        EnableImportCompletion = true,
-                        -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-                        -- true
-                        AnalyzeOpenDocumentsOnly = nil,
-                    },
-                    Sdk = {
-                        -- Specifies whether to include preview versions of the .NET SDK when
-                        -- determining which version to use for project loading.
-                        IncludePrereleases = true,
-                    },
-                },
-
-                capabilities = capabilities,
-                on_attach = on_attach
-            }
         end
     }
 }
