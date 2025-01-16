51386edf3a96d311f240cccb22e1ffa92c2e7cb8
Paul Fesenmeier <pfesenmeier@gmail.com>
2025-01-15 23:32:34 -0500

    try non-hybrid vue mode

--- neovim/lua/pfes/vue.lua
+++ neovim/lua/pfes/vue.lua
@@ -30,36 +30,51 @@ vue.addToDap = function(dap)
 end
 
 vue.addToLspConfig = function(opts, capabilities, on_attach)
-    local util = require 'lspconfig.util'
     opts.volar.setup {
         capabilities = capabilities,
-        on_attach = on_attach
-    }
-    opts.ts_ls.setup {
-        -- package.json for ts_ls, deno.json for deno
-        root_dir = util.root_pattern('package.json'),
-        single_file_support = false,
+        on_attach = on_attach,
+        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
         init_options = {
-            plugins = {
-                {
-                    name = "@vue/typescript-plugin",
-                    location = env.home .. "/.local/share/fnm/node-versions/v20.11.1/installation/lib/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin/",
-                    languages = {
-                        "javascript",
-                        "typescript",
-                        "vue"
-                    },
-                },
-            },
-        },
-        filetypes = {
-            "javascript",
-            "typescript",
-            "vue",
-        },
-        capabilities = capabilities,
-        on_attach = on_attach
+          vue = {
+            hybridMode = false,
+          },
+          tsdk = '/home/paul/.local/share/fnm/node-versions/v20.11.1/installation/lib/node_modules/typescript/lib'
+        }
+
     }
 end
+
+-- vue.addToLspConfig = function(opts, capabilities, on_attach)
+--     local util = require 'lspconfig.util'
+--     opts.volar.setup {
+--         capabilities = capabilities,
+--         on_attach = on_attach
+--     }
+--     opts.ts_ls.setup {
+--         -- package.json for ts_ls, deno.json for deno
+--         root_dir = util.root_pattern('package.json'),
+--         single_file_support = false,
+--         init_options = {
+--             plugins = {
+--                 {
+--                     name = "@vue/typescript-plugin",
+--                     location = env.home .. "/.local/share/fnm/node-versions/v20.11.1/installation/lib/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin/",
+--                     languages = {
+--                         "javascript",
+--                         "typescript",
+--                         "vue"
+--                     },
+--                 },
+--             },
+--         },
+--         filetypes = {
+--             "javascript",
+--             "typescript",
+--             "vue",
+--         },
+--         capabilities = capabilities,
+--         on_attach = on_attach
+--     }
+-- end
 
 return vue
