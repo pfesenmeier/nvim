7c5471e97fa91be4803aeeea762fe4028c374e7d
Paul Fesenmeier <pfesenmeier@gmail.com>
2025-01-25 22:09:56 -0500

    cleanup spec1

--- neovim/lua/plugins/spec1.lua
+++ neovim/lua/plugins/spec1.lua
@@ -2,6 +2,9 @@ local env = require "pfes.env"
 
 local islinux = env.islinux
 
+local sql = false
+local c_sharp = false
+
 local packages = {
     -- prevent remote code execution
     { "ciaranm/securemodelines",       lazy = true, event = "VeryLazy" },
@@ -20,56 +23,23 @@ local packages = {
         lazy = false,
         event = "UIEnter",
         init = function()
-            -- vim.g.rooter_patterns = { 'package.json', 'deno.json', '.git', 'Makefile', '*.sln', 'build/env.sh' }
-            vim.g.rooter_patterns = { '.git', 'Makefile', '*.sln', 'build/env.sh' }
+            vim.g.rooter_patterns = { '.git', 'package.json', 'deno.json', '*.sln'  }
         end
     },
-    { "editorconfig/editorconfig-vim", lazy = true, enabled = false, event = "VeryLazy" },
     -- database
     {
         "tpope/vim-dadbod",
         ft = "sql",
         lazy = true,
-        enabled = false,
+        enabled = sql,
         dependencies = { "kristijanhusak/vim-dadbod-completion" }
     },
     -- workspace errors
-    {
-        "folke/trouble.nvim",
-        enabled = false,
-        tag = 'v2.10.0',
-        opts = {
-            -- settings without a patched font or icons
-            icons = false,
-            fold_open = "v",   -- icon used for open folds
-            fold_closed = ">", -- icon used for closed folds
-            -- indent_lines = true, -- add an indent guide below the fold icons
-            signs = {
-                -- icons / text used for a diagnostic
-                error = "error",
-                warning = "warn",
-                hint = "hint",
-                information = "info"
-            },
-            -- use_diagnostic_signs = true -- enabling this will use the signs defined in your lsp client
-        },
-        init = function()
-            -- :h Trouble
-            vim.keymap.set("n", "<leader>ox", function() require("trouble").toggle() end)
-            vim.keymap.set("n", "<leader>ow",
-                function() require("trouble").toggle("workspace_diagnostics") end)
-            vim.keymap.set("n", "<leader>od",
-                function() require("trouble").toggle("document_diagnostics") end)
-            vim.keymap.set("n", "<leader>oq", function() require("trouble").toggle("quickfix") end)
-            vim.keymap.set("n", "<leader>ol", function() require("trouble").toggle("loclist") end)
-            vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
-        end
-    },
     -- inspect decompiled C#
     {
         "Hoffs/omnisharp-extended-lsp.nvim",
         lazy = true,
-        enabled = false,
+        enabled = c_sharp,
         ft = "cs"
     },
 
@@ -90,51 +60,7 @@ local packages = {
     -- git
     {
         "lewis6991/gitsigns.nvim",
-        opts = {
-            on_attach = function(bufnr)
-                local gs = package.loaded.gitsigns
-
-                local function map(mode, l, r, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, l, r, opts)
-                end
-
-                -- Navigation
-                map('n', ']c', function()
-                    if vim.wo.diff then return ']c' end
-                    vim.schedule(function() gs.next_hunk() end)
-                    return '<Ignore>'
-                end, { expr = true })
-
-                map('n', '[c', function()
-                    if vim.wo.diff then return '[c' end
-                    vim.schedule(function() gs.prev_hunk() end)
-                    return '<Ignore>'
-                end, { expr = true })
-
-                -- Actions
-                map('n', '<leader>hs', gs.stage_hunk)
-                map('n', '<leader>hr', gs.reset_hunk)
-                map('v', '<leader>hs',
-                    function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-                map('v', '<leader>hr',
-                    function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-                map('n', '<leader>hS', gs.stage_buffer)
-                map('n', '<leader>hu', gs.undo_stage_hunk)
-                map('n', '<leader>hR', gs.reset_buffer)
-                map('n', '<leader>hp', gs.preview_hunk)
-                map('n', '<leader>hb', function() gs.blame_line { full = true } end)
-                map('n', '<leader>tb', gs.toggle_current_line_blame)
-                map('n', '<leader>hd', gs.diffthis)
-                map('n', '<leader>hD', function() gs.diffthis('~') end)
-                --    map('n', '<leader>td', gs.toggle_deleted)
-
-                -- Text object
-                map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-            end
-        },
-        enabled = true,
+        opts = {},
         tag = "v0.9.0",
         lazy = true,
         event = "VeryLazy"
@@ -183,38 +109,20 @@ local packages = {
         opts = {},
     },
     {
-        "zootedb0t/citruszest.nvim",
-        lazy = false,
-        init = function()
-            vim.cmd("colorscheme citruszest")
-        end,
-        enabled = false,
-        priority = 1000,
-    },
-    {
         'ellisonleao/gruvbox.nvim',
         priority = 1000,
-        enabled = true,
         init = function()
             vim.cmd("colorscheme gruvbox")
         end,
         opts = {
+            palette_overrides = {
+              dark0_hard = "#000000",
+            },
             contrast = "hard", -- can be "hard", "soft" or empty string
-            transparent_mode = true,
         }
-    }, { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', enabled = true },
-    {
-        'stevearc/oil.nvim',
-        opts = {
-            default_file_explorer = islinux
-        },
-        lazy = true,
-        enabled = false,
-        event = "VeryLazy"
-    },
+    }, { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     {
         "lambdalisue/fern.vim",
-        enabled = true,
         dependencies = {
             "lambdalisue/vim-fern-hijack",
             "yuki-yano/fern-preview.vim"
