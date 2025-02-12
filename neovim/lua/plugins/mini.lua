ab0a353247157d073dd68bcf0e301de00979aa7c
Paul Fesenmeier <pfesenmeier@gmail.com>
2025-02-11 22:00:42 -0500

    mini.files

--- neovim/lua/plugins/mini.lua
+++ neovim/lua/plugins/mini.lua
@@ -8,7 +8,10 @@ return {
       require 'mini.statusline'.setup {
         use_icons = true
       }
-      require 'mini.surround'.setup()
+      -- require 'mini.surround'.setup()
+      require 'mini.files'.setup()
+
+      vim.keymap.set('n', '<leader>m', function() require('mini.files').open() end, {})
     end
   },
 }
