e7c225cf0e835fc1297061593a8d28dceb6520c6
Paul Fesenmeier <pfesenmeier@gmail.com>
2025-05-24 03:17:14 +0000

    add start.sh

---
+++ start.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+# curl -s https://raw.githubusercontent.com/pfesenmeier/nvim/main/start.ps1 | bash
+
+# install brew if not installed - see docs
+# https://docs.brew.sh/Homebrew-on-Linux#install
+if command -v brew &> /dev/null; then
+    echo "brew already installed."
+else
+  echo "This script needs administrator priviledges to install brew prerequisites:"
+  packages="build-essential procps curl file git"
+  echo "$packages"
+  sudo apt update && sudo apt install -y $packages
+  test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+  test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+fi
+
+if [ -d "$HOME/nvim" ]; then
+    echo "~/nvim folder already exists"
+else
+    cd "$HOME"
+    git clone https://github.com/pfesenmeier/nvim
+fi
