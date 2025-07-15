ebd42b81545952069912b6e8a15a560272089acf
Paul Fesenmeier <pbfesenmeier@sep.com>
2025-07-15 13:47:20 -0400

    add mini.ai, bufremove options

--- neovim/lua/plugins/mini.lua
+++ neovim/lua/plugins/mini.lua
@@ -3,11 +3,25 @@ return {
     'echasnovski/mini.nvim',
     enabled = true,
     config = function()
-      require "mini.ai".setup()
+      local gen_spec = require('mini.ai').gen_spec
+      require "mini.ai".setup({
+        custom_textobjects = {
+          -- Function definition (needs treesitter queries with these captures)
+          F = gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }),
+          o = gen_spec.treesitter({
+            a = { '@conditional.outer', '@loop.outer' },
+            i = { '@conditional.inner', '@loop.inner' },
+          })
+        }
+      })
       require "mini.bufremove".setup()
       require 'mini.statusline'.setup {
         use_icons = true
       }
+
+      vim.api.nvim_create_user_command("Bd", function()
+        require('mini.bufremove').delete()
+      end, {})
       -- require 'mini.surround'.setup()
       require 'mini.files'.setup()
       require 'mini.starter'.setup()
