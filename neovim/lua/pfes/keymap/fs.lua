82a6f22e2f1f974973eba4623887a0fe93a37deb
Paul Fesenmeier <pfesenmeier@gmail.com>
2025-09-06 17:27:05 -0400

    fix homescreen, install script, tool install cmd

--- neovim/lua/pfes/keymap/fs.lua
+++ neovim/lua/pfes/keymap/fs.lua
@@ -2,18 +2,18 @@ local M = {}
 local opts = { noremap = true, silent = true }
 
 M.setup = function()
-local function go_up()
-  local bufname = vim.api.nvim_buf_get_name(0)
-  if bufname == "" or string.match(bufname, "fugitive:") then
-    vim.cmd('e .')
-  elseif vim.o.buftype == "terminal" then
-    vim.cmd('e .')
-  else
-    vim.cmd('e %:h')
+  local function go_up()
+    local bufname = vim.api.nvim_buf_get_name(0)
+    if bufname == ""
+        or string.match(bufname, "fugitive:")
+        or string.match(bufname, "ministarter:")
+        or vim.o.buftype == "terminal" then
+      vim.cmd('e .')
+    else
+      vim.cmd('e %:h')
+    end
   end
-end
-vim.keymap.set('n', '<leader>e', go_up, opts)
-
+  vim.keymap.set('n', '<leader>e', go_up, opts)
 end
 
 return M
--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
+# installs brew, nushell, and neovim, and this repository
+
 # curl -s https://raw.githubusercontent.com/pfesenmeier/nvim/main/start.sh | bash
 
-# install brew if not installed - see docs
-# https://docs.brew.sh/Homebrew-on-Linux#install
 if command -v brew &> /dev/null; then
     echo "brew already installed."
 else
@@ -11,10 +11,16 @@ else
   packages="build-essential procps curl file git"
   echo "$packages"
   sudo apt update && sudo apt install -y $packages
+
+  eche "Running homebrew install script"
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
   test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
   test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
 fi
+
+brew install neovim nushell
 
 if [ -d "$HOME/nvim" ]; then
     echo "~/nvim folder already exists"
@@ -22,3 +28,5 @@ else
     cd "$HOME"
     git clone https://github.com/pfesenmeier/nvim
 fi
+
+
