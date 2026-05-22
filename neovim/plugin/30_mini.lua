f64b101a9e4cc8b4287f2c7b1c74f866cf201021
Paul Fesenmeier <Paul.Fesenmeier@lids.com>
2026-05-22 12:32:40 -0400

    mini picker select

--- neovim/plugin/30_mini.lua
+++ neovim/plugin/30_mini.lua
@@ -669,7 +669,11 @@ end)
 -- - `:h MiniPick.builtin` and `:h MiniExtra.pickers` - available pickers;
 --   Execute one either with Lua function, `:Pick <picker-name>` command, or
 --   one of `<Leader>f` mappings defined in 'plugin/20_keymaps.lua'
-later(function() require('mini.pick').setup() end)
+later(function()
+  require('mini.pick').setup({
+    mappings = { choose_marked = '<C-y>' },
+  })
+end)
 
 -- Manage and expand snippets (templates for a frequently used text).
 -- Typical workflow is to type snippet's (configurable) prefix and expand it
