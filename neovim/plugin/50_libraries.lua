41f39e26c2a4dc2aed15684cbf8801ede14661b9
Paul Fesenmeier <pfesenmeier@gmail.com>
2026-07-18 15:33:16 -0400

    not producing error (but untested)

--- neovim/plugin/50_libraries.lua
+++ neovim/plugin/50_libraries.lua
@@ -1,15 +1,9 @@
 -- Setup calls for custom libraries under neovim/lua/.
 -- Add new modules here rather than creating a per-module plugin/ file.
 
-require("remote").setup()
-
 local later = Config.later
 
 later(function()
-  -- Exports HueyBuffer
   require("huey.buffer").setup()
-  -- Exports HueyTerm
-  require("huey.term").setup()
-
   require("workspace").setup({})
 end)
